e6463daf555c9506f83c92ae384f68947d36704d
dprakash2101 <dprakash2101@gmail.com>
2024-09-04 19:56:06 +0530

    Add project files.

---
+++ Otp_Generation/OtpGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace OtpGenerationSdk
+{
+    public class OtpGenerator
+    {
+        public static string GenerateOtp(int length, string secretKey)
+        {
+            if (length <= 0)
+            {
+                throw new ArgumentOutOfRangeException("OTP length must be greater than 0.");
+            }
+
+            if (string.IsNullOrEmpty(secretKey))
+            {
+                throw new ArgumentNullException("Secret key cannot be null or empty.");
+            }
+
+            using (var hmac = new HMACSHA256(Encoding.UTF8.GetBytes(secretKey)))
+            {
+                var counter = BitConverter.GetBytes(DateTime.UtcNow.Ticks);
+                var hash = hmac.ComputeHash(counter);
+
+                var otp = new StringBuilder();
+                for (int i = 0; i < length; i++)
+                {
+                    otp.Append((hash[i % hash.Length] % 10).ToString()); 
+                }
+
+                return otp.ToString();
+            }
+        }
+    }
+}
